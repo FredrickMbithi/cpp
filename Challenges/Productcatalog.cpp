1f68ec7e1205e8d0eae7f2bd7c33a25d58b777d2
FredrickMbithi <fredrickmbithi000@gmail.com>
2025-11-24 14:10:31 +0300

    Refactor code structure for improved readability and maintainability

--- Challenges/Productcatalog.cpp
+++ Challenges/Productcatalog.cpp
@@ -9,15 +9,34 @@ public:
     double quantity;
     std::string name;
 
+    // Method 1: Display info (I added ": " for better spacing)
     void display()
     {
         std::cout << "Price" << price << std::endl;
         std::cout << "Quantity" << quantity << std::endl;
         std::cout << "Name" << name << std::endl;
     }
+    // Method 2: Calculate Total Value (The missing requirement)
+    double calculate_total_value()
+    {
+        return price * quantity;
+    }
 };
 
 int main()
 {
     // creating objects (instances)
+    Product product1;
+    // Passing values to the attributes
+    product1.name = "Tea leaves";
+    product1.price = 20;
+    product1.quantity = 20;
+
+    // 2. Call the display method
+    product1.display();
+    // call the new method
+    double total = product1.calculate_total_value();
+    std::cout << "Total Stock value: " << total << std::endl;
+
+    return 0;
 }
